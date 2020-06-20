8c7664912dea950e252009c965399abd8fd84081
AllexxT <sanya92t@gmail.com>
2020-06-21 00:04:56 +0300

    another commit

--- tsite/frontend/urls.py
+++ tsite/frontend/urls.py
@@ -5,7 +5,14 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('novosti/', views.news, name='news'),
-    path('nashi-uslugi/', views.services, name='services'),
+    # Services
+    path('nashi-uslugi/', views.uslugi, name='uslugi'),
+    path('nashi-uslugi/ukladka-trotuarnoj-plitki/', views.ustanovka, name='ukladka-plitki'),
+    path('nashi-uslugi/ustanovka-evrozabora/', views.ustanovka, name='ustanovka-evrozabora'),
+    path('nashi-uslugi/ustanovka-pamyatnikov/', views.ustanovka, name='ustanovka-pamyatnikov'),
+    path('nashi-uslugi/dostavka/', views.dostavka, name='dostavka'),
+    path('nashi-uslugi/zamer/', views.zamer, name='zamer'),
+    # !
     path('sertifikaty/', views.sertificates, name='sertificates'),
     path('nashi-raboty/', views.our_works, name='our_works'),
     # path('trotuarnaya-plitka-zaporozhe/', views.news, name='sett'),
--- tsite/frontend/views.py
+++ tsite/frontend/views.py
@@ -9,13 +9,12 @@ pp = pprint.PrettyPrinter(width=80, compact=True)
 
 
 def index(request):
-    pp.pprint(request.path)
+    # pp.pprint(request.path)
     pageHead = Page.objects.get(page='products')
     return render(request, 'frontend/index.html', {'seo': pageHead})
 
 
 def news(request):
-    pp.pprint(request.path)
     pageHead = Page.objects.get(page='novosti')
     news = News.objects.all()
     return render(
@@ -25,14 +24,7 @@ def news(request):
     )
 
 
-def services(request):
-    pp.pprint(request.path)
-    pageHead = Page.objects.get(page='services')
-    return render(request, 'frontend/services.html', {'seo': pageHead})
-
-
 def sertificates(request):
-    pp.pprint(request.path)
     pageHead = Page.objects.get(page='sertificates')
     sertificates = Sertificat.objects.all()
     return render(
@@ -43,6 +35,32 @@ def sertificates(request):
 
 
 def our_works(request):
-    pp.pprint(request.path)
     pageHead = Page.objects.get(page='our_works')
     return render(request, 'frontend/our_works.html', {'seo': pageHead})
+
+
+def uslugi(request):
+    pageHead = Page.objects.get(page='nashi-uslugi')
+    return render(request, 'frontend/services.html', {'seo': pageHead})
+
+
+def ustanovka(request):
+    # pp.pprint(request.path.split('/')[-2])
+    path = request.path.split('/')[-2]
+    if path == "ukladka-trotuarnoj-plitki":
+        pageHead = Page.objects.get(page='ukladka-trotuarnoj-plitki')
+    if path == "ustanovka-evrozabora":
+        pageHead = Page.objects.get(page='ustanovka-evrozabora')
+    if path == "ustanovka-pamyatnikov":
+        pageHead = Page.objects.get(page='ustanovka-pamyatnikov')
+    return render(request, 'frontend/installation.html', {'seo': pageHead})
+
+
+def dostavka(request):
+    pageHead = Page.objects.get(page='dostavka')
+    return render(request, 'frontend/dostavka.html', {'seo': pageHead})
+
+
+def zamer(request):
+    pageHead = Page.objects.get(page='zamer')
+    return render(request, 'frontend/zamer.html', {'seo': pageHead})
